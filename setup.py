8c1453ce2ed100a0d6c42eb5773c498f206e7cde
freeman <the.freeman.lab@gmail.com>
2015-11-08 00:01:45 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import station
+
+setup(
+    name='station',
+    version=station.__version__,
+    description='context manager computational engines',
+    author='freeman-lab',
+    author_email='the.freeman.lab@gmail.com',
+    packages=['station'],
+    url='https://github.com/freeman-lab/station',
+    long_description='See https://github.com/freeman-lab/station'
+)
